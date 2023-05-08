89f9e7e2a1ba416ff77a313742db4621f0a3146d
endanghendayatna <a2.2000125@mhs.stmik-sumedang.ac.id>
2023-05-08 19:03:18 +0700

    Resutl Ch 5

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "endang",
+    database: "database_development",
+    host: "localhost",
+    dialect: "postgres"
+  },
+}
